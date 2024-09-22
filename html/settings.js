c538bc7bde1fcd2d5a2cb8f2ba76c1bfa2018c45
Kolo2415 <krzysztoftanski278@gmail.com>
2024-09-22 16:43:40 +0200

    update

--- html/settings.js
+++ html/settings.js
@@ -1,1 +1,7 @@
-console.log('xd');
+document.addEventListener('DOMContentLoaded', () => {
+  const settingsElement = document.querySelector('body div div label');
+
+  settingsElement.addEventListener('click', () => {
+    document.querySelector('body').classList.toggle('dark');
+  });
+});
