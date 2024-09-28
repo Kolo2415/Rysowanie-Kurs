68c0729130bb5695367f85896de5542c7f13ba7d
Kolo2415 <krzysztoftanski278@gmail.com>
2024-09-28 20:52:50 +0200

    update

--- settings.js
+++ settings.js
@@ -5,3 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('body').classList.toggle('dark');
   });
 });
+document.addEventListener('DOMContentLoaded', () => {
+  // Wybieramy burgera i menu
+  const burgerElement = document.querySelector('.burger input');
+  const menuElement = document.querySelector('#menu ul');
+
+  // Nasłuchujemy na kliknięcie burgera
+  burgerElement.addEventListener('click', () => {
+    // Sprawdzamy, czy menu jest widoczne i przełączamy między `none` a `flex`
+    if (menuElement.style.display === 'flex') {
+      menuElement.style.display = 'none';
+      console.log('xd');
+    } else {
+      menuElement.style.display = 'flex';
+    }
+  });
+});
